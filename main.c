7a85b97d2a07db9edd7f07c0214b6d32f44bd4b8
Anna Norris <alnorris113@gmail.com>
2026-03-18 17:33:45 -0700

    add lexer and functions to read char

--- main.c
+++ main.c
@@ -1,1 +1,90 @@
-hi
+/*
+Created by following guide by AlexTheRealDev
+Video: How to Build a Compiler from Scratch | Full Guide
+https://www.youtube.com/watch?v=HOe2YFnzO2I
+*/
+
+#include <stdio.h>
+#define DS_IO_IMPLEMENTATION
+#include "ds.h"
+
+enum token_kind {
+    IDENT,
+    LABEL,
+    INT,
+    INPUT,
+    OUTPUT,
+    GOTO,
+    IF,
+    THEN,
+    EQUAL,
+    PLUS,
+    LESS_THAN,
+    INVALID
+};
+
+const char *show_token(enum token_kind kind) {
+    switch(kind) {
+        case IDENT: 
+            return "ident";
+        case LABEL: 
+            return "label";
+        case INT: 
+            return "int";
+        case INPUT: 
+            return "input";
+        case OUTPUT: 
+            return "output";
+        case GOTO: 
+            return "goto";
+        case IF: 
+            return "if";
+        case THEN: 
+            return "then";
+        case EQUAL: 
+            return "equal";
+        case PLUS: 
+            return "plus";
+        case LESS_THAN: 
+            return "less_than";
+        case INVALID:
+            return "invalid";
+    }
+}
+
+struct lexer {
+    char *buffer;
+    unsigned int buffer_len;
+    unsigned int pos;
+    unsigned int read_pos;
+    char ch;
+};
+
+/*
+looks at current read position and returns the 
+character at that position
+*/
+static char lexer_peek_char(struct lexer *l) {
+    if (l->read_pos >= l->buffer_len) {
+        return EOF;
+    }
+
+    return l->buffer[l->read_pos];
+}
+
+static char lexer_read_char(struct lexer *l) {
+    l->ch = lexer_peek_char(l);
+
+    l->pos = l->read_pos;
+    l->read_pos += 1;
+
+    return l->ch;
+}
+
+int main() {
+    char *buffer = NULL;
+    ds_io_read_file(NULL, &buffer);
+
+    printf("%s\n", buffer);
+    return 0;
+}
